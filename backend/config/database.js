bd34c76a7e6f98785f8aca82a573c4ef4e42b0c4
Rakshata <rakshataaaa@gmail.com>
2026-08-15 23:43:28 +0530

    Secure database password placeholder

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root123',        
+    password: 'your_password_here',        
     database: 'skillswapy',
     waitForConnections: true,
     connectionLimit: 10,
